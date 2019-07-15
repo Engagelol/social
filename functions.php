7c50547e42ef9588b20bbea1ba83c1cce96a1576
Engagelol <audoken2125@gmail.com>
2019-07-15 17:10:17 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,26 +6,26 @@
 
   $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
   if ($connection->connect_error) die("Fatal Error");
-
+  //Проверяет факт существование таблицы и создает отсутствующую
   function createTable($name, $query) {
     queryMysql("CREATE TABLE IF NOT EXISTS $name($query)");
     echo "Таблица '$name' создана или уже существует<br>";
   }
-
+  //Запрос к базе данных или ошибка при сбое
   function queryMysql($query) {
     global $connection;
     $result = $connection->query($query);
     if (!$result) die("Fatal Error");
     return $result;
   }
-
+  //Уничтожение сессии
   function destroySession() {
     $_SESSION = array();
     if (session_id() != "" || isset($_COOKIE[session_name()]))
       setcookie(session_name(), '', time()-2592000, '/');
     session_destroy();
   }
-
+  //Проверка на вредоносный код
   function sanitizeString($var) {
     global $connection;
     $var = strip_tags($var);
@@ -34,7 +34,7 @@
       $var = stripslashes($var);
     return $connection->real_escape_string($var);
   }
-
+  //Отображает профиль
   function showProfile($user) {
     if(file_exists("$user.jpg"))
       echo "<img src='$user.jpg' align='left'>";
